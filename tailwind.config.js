581d8d27274a5ccf5183c90455e78bfbda923736
AAminha <dks4857@gmail.com>
2024-12-06 17:36:22 +0900

    chore: 로컬 디자인 시스템

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        default: {
+          white: "#FFFFFF",
+          black: "#000000",
+        },
+        primary: {
+          default: "#9694FF",
+          light: "#EBEAFF",
+          accent: "#3D3BF3",
+          hover: "#8280FF",
+        },
+        secondary: {
+          warning: "#FF2929",
+        },
+        gray: {
+          100: "#0A0A0A",
+          95: "#161616",
+          90: "#232323",
+          80: "#3B3B3B",
+          70: "#545454",
+          60: "#6C6C6C",
+          50: "#858585",
+          40: "#9D9D9D",
+          30: "#B6B6B6",
+          20: "#CECECE",
+          15: "#DADADA",
+          10: "#E7E7E7",
+          5: "#F2F2F2",
+        },
+      },
+    },
   },
   plugins: [],
   future: {
