192f04a296ef31e1e6f2341ff6a0b9ef6f3ef259
AAminha <dks4857@gmail.com>
2024-12-06 21:29:01 +0900

    feat: Register 페이지 구현

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router";
+import { Outlet } from 'react-router';
+import Header from './Header';
 
 export default function RootLayout() {
   return (
     <>
-      {/* 추후에 Header 컴포넌트로 대체 */}
-      <header>header</header>
-      <Outlet />
+      <Header />
+      <main className="w-full">
+        <Outlet />
+      </main>
     </>
   );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,3 +1,97 @@
+import { useState } from 'react';
+import InputLabel from '../components/common/InputLabel';
+import Button from '../components/common/Button';
+import Logo from '../assets/logo.svg';
+
 export default function Register() {
-  return <div>Register</div>;
+  const [name, setName] = useState({ value: '', isWarning: false });
+  const [email, setEmail] = useState({ value: '', isWarning: false });
+  const [password, setPassword] = useState({ value: '', isWarning: false });
+
+  const validate = () => {
+    if (!name.value) {
+      setName({ ...name, isWarning: true });
+    }
+    if (!email.value) {
+      setEmail({ ...email, isWarning: true });
+    }
+    if (!password.value) {
+      setPassword({ ...password, isWarning: true });
+    }
+
+    if (!name.value || !email.value || !password.value) {
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!validate()) return;
+    console.log(name, email, password); // TODO: 회원등록 API 연동
+  };
+
+  return (
+    <section className="flex h-screen items-center justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col w-[400px] gap-[45px]">
+        <img
+          src={Logo}
+          alt="로고"
+          className="mx-auto w-[100px] h-[100px]"
+        />
+        <div>
+          <InputLabel
+            label="이름"
+            id="name"
+            type="text"
+            value={name.value}
+            message="이름을 입력해주세요"
+            isWarning={name.isWarning}
+            onChange={(e) =>
+              setName({ value: e.target.value, isWarning: false })
+            }
+          />
+          <InputLabel
+            label="이메일"
+            id="email"
+            type="email"
+            value={email.value}
+            message="이메일을 입력해주세요"
+            isWarning={email.isWarning}
+            onChange={(e) =>
+              setEmail({ value: e.target.value, isWarning: false })
+            }
+          />
+          <InputLabel
+            label="비밀번호"
+            id="password"
+            type="password"
+            value={password.value}
+            message="비밀번호를 입력해주세요"
+            isWarning={password.isWarning}
+            onChange={(e) =>
+              setPassword({ value: e.target.value, isWarning: false })
+            }
+          />
+        </div>
+        <div className="flex flex-col gap-5">
+          <Button
+            primary
+            type="submit">
+            회원 등록
+          </Button>
+          <Button
+            type="button"
+            onClick={() => {
+              console.log('취소');
+              //TODO: 취소 작업 해야함
+            }}>
+            취소
+          </Button>
+        </div>
+      </form>
+    </section>
+  );
 }
