fb349eba5d50b7ea48739d2a2a14c7919c40267b
AAminha <dks4857@gmail.com>
2024-12-11 02:46:36 +0900

    style: ProfileSection 컴포넌트에서 프로필 이미지가 없을 경우에 대한 디폴트 버전 추가

--- src/components/Profile/ProfileSection.tsx
+++ src/components/Profile/ProfileSection.tsx
@@ -3,6 +3,7 @@ import ProfileTemp from "../../assets/profileImg.jpg";
 import { USER_INFO } from "../../constants/profile";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
+import Logo from "../../assets/Logo";
 
 const TEMP_USER = {
   name: "음악하는 다람쥐",
@@ -14,19 +15,28 @@ const TEMP_USER = {
   },
 };
 
-export default function ProfileSection() {
-  const isMyProfile = !true; // 임시
+export default function ProfileSection({
+  isMyProfile = false,
+}: {
+  isMyProfile?: boolean;
+}) {
   const [isFollow, setIsFollow] = useState(false);
 
   return (
     <article className="border-b border-gray-ee flex justify-center items-center gap-20 mb-10 p-10 w-full">
       {/* 정훈님 컴포넌트 재사용? */}
       <div className="flex flex-col gap-2 items-center">
-        <img
-          src={TEMP_USER.image}
-          alt="프로필 이미지"
-          className="w-[122px] h-[122px] rounded-full profile-shadow"
-        />
+        {TEMP_USER.image ? (
+          <img
+            src={TEMP_USER.image}
+            alt="프로필 이미지"
+            className="w-[124px] h-[124px] rounded-full profile-shadow border border-gray-ee"
+          />
+        ) : (
+          <div className="w-[124px] h-[124px] rounded-full flex items-center justify-center border border-gray-ee profile-shadow">
+            <Logo className="h-16 w-16" />
+          </div>
+        )}
         <p className="text-lg mt-[10px]">{TEMP_USER.name}</p>
       </div>
       <section className="w-max">
