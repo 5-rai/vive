351205bf9c5facb3e4110b1fe3e2f014f4c71ea1
dilma01 <dilma01@naver.com>
2024-12-11 01:47:17 +0900

    🚨 Fix: [SearchButton 제외한 모든 Button들] - [Link를 컴포넌트 내로 이동]

--- src/components/Header/LogoButton.tsx
+++ src/components/Header/LogoButton.tsx
@@ -1,9 +1,12 @@
 import Logo from "../../assets/Logo";
+import { Link } from "react-router";
 
 export default function LogoButton() {
   return (
-    <button className="flex-grow-0 flex-shrink-0 w-[45px] h-[45px]">
-      <Logo className="w-[45px] h-[45px] absolute left-[39.5px] top-[11.5px] object-cover" />
-    </button>
+    <Link to="/">
+      <button className="flex-grow-0 flex-shrink-0 w-[45px] h-[45px]">
+        <Logo className="w-[45px] h-[45px] absolute left-[39.5px] top-[11.5px] object-cover" />
+      </button>
+    </Link>
   );
 }
