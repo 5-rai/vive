f2554474361fbd157e93f064edc49db6f466010b
wjh1010 <42775656+wjh1010@users.noreply.github.com>
2024-12-11 11:19:47 +0900

    💄 style/Home-page

--- src/components/Home/RecentPosts.tsx
+++ src/components/Home/RecentPosts.tsx
@@ -1,23 +1,35 @@
-export default function RecentPosts() {
+export default function RecentPosts({
+  artist,
+  songTitle,
+  youtubeTitle,
+  youtubeThumbnail,
+  avatarImg,
+}: {
+  artist: string;
+  songTitle: string;
+  youtubeTitle: string;
+  youtubeThumbnail: string;
+  avatarImg: string;
+}) {
   return (
-    <div className="w-[208px] h-[276px] rounded-lg flex-col justify-start items-start inline-flex">
+    <div className="border-t-[24px] border-t-transparent w-[208px] h-[276px] rounded-lg flex-col justify-start items-start inline-flex">
       <div className="h-[178px] relative bg-gradient-to-b from-black to-black rounded-tl-lg rounded-tr-lg border-r border-[#ededed]">
-       {/* 유튜브 썸네일 추가  */}
+        <img src={youtubeThumbnail} />
         <div className="left-[8px] top-[146px] absolute justify-start items-end gap-3 inline-flex">
           <div className="w-6 h-6 justify-center items-center flex">
-            <img className="w-6 h-6" src="" />
+            <img className="w-6 h-6" src={avatarImg} />
           </div>
-          <div className="text-[#6c6c6c] text-sm font-medium font-['Pretendard'] leading-snug">
+          <div className="text-gray-c8 text-sm font-medium leading-snug">
             avatar
           </div>
         </div>
       </div>
       <div className="self-stretch h-[98px] p-2 bg-white rounded-bl-lg rounded-br-lg border-l border-r border-b border-[#ededed] flex-col justify-start items-start gap-2 flex">
-        <div className="self-stretch text-[#222222] text-base font-semibold font-['Pretendard'] uppercase leading-[30px]">
-          이무진 - 청춘만화
+        <div className="self-stretch text-[#222222] text-base font-semibold uppercase leading-[30px]">
+          {`${artist}-${songTitle}`}
         </div>
-        <div className="self-stretch text-[#c8c8c8] text-sm font-normal font-['Pretendard'] uppercase leading-snug">
-          해당 음원 유튜브 영상 제목 포스트 해당 음원 유튜브 영상 제목 ...
+        <div className="self-stretch text-[#c8c8c8] text-sm font-normal uppercase leading-snug">
+          {youtubeTitle}
         </div>
       </div>
     </div>
--- src/components/Home/WeeklyArtist.tsx
+++ src/components/Home/WeeklyArtist.tsx
@@ -6,14 +6,14 @@ export default function WeeklyArtist({
   images: string;
 }) {
   return (
-    <div className="h-[162px] justify-between items-start inline-flex">
+    <div className="border-t-[24px] border-t-transparent h-[162px] justify-between items-start inline-flex">
       <div className="w-[122px] relative">
         <img
           className="w-[122px] h-[122px] left-0 top-0 absolute rounded-full shadow"
           src={images}
         />
         <div className="w-[122px] h-[30px] left-0 top-[132px] absolute justify-start items-center gap-[30px] inline-flex">
-          <div className="w-[261px] text-[#222222] text-lg font-normal font-['Pretendard'] uppercase leading-[30px]">
+          <div className="whitespace-nowrap w-[261px] text-[#222222] text-lg font-normal uppercase leading-[30px]">
             {name}
           </div>
         </div>
