ede5af5fff6caf502c9ae5b1280f32ce08249465
AAminha <dks4857@gmail.com>
2024-12-13 15:24:13 +0900

    Style: 마이페이지 데이터 로딩 및 에러 발생 시 보여지는 화면 퍼블리싱

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -25,12 +25,23 @@ export default function MyProfile() {
     fetchUserData();
   }, []);
 
-  if (loading) return <div>로딩 중</div>;
-  if (error) return <div>{error}</div>;
-  if (!user) return <div>사용자 정보를 찾을 수 없습니다.</div>;
+  if (loading || error || !user)
+    return (
+      <section className="w-[934px] mx-auto flex items-center justify-center">
+        {loading && (
+          <div>
+            <div className="loader" />
+          </div>
+        )}
+        {error && <p className="text-lg font-medium">{error}</p>}
+        {!user && (
+          <p className="text-lg font-medium">사용자 정보를 찾을 수 없습니다.</p>
+        )}
+      </section>
+    );
 
   return (
-    <section className="w-fit mx-auto flex flex-col items-center">
+    <section className="w-[934px] mx-auto flex flex-col items-center">
       <ProfileSection user={user} isMyProfile={true} />
       <PostCardGridSection posts={user.posts} />
     </section>
