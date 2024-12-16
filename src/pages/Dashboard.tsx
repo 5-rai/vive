d9a7745613946e6c0c80ea2db917a561ef0be3a8
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-12-17 02:49:50 +0900

    Fix: 채널 이름이 제대로 들어오지 않을 경우, 무한로딩이 되는 버그 수정

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,18 +7,18 @@ import Loading from "../components/common/Loading";
 
 export default function Dashboard() {
   const { channelName } = useParams();
-  const channels = useChannelStore((state) => state.channels);
+  const getIdFromName = useChannelStore((state) => state.getIdFromName);
   const [posts, setPosts] = useState<Post[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   // 선택된 채널 찾기 및 해당 채널의 포스트 불러오기
   useEffect(() => {
-    const channel = channels.find((c) => c.name === channelName);
-    if (channel) {
-      fetchPosts(channel._id);
-    }
-  }, [channelName, channels]);
+    if (!channelName) return;
+
+    const channelId = getIdFromName(channelName);
+    if (channelId) fetchPosts(channelId);
+  }, []);
 
   // 선택된 채널의 포스트 목록 API 요청
   const fetchPosts = async (channelId: string) => {
