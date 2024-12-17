af4686bbd70bd97991e3799ff8bff67947e0d25c
AAminha <dks4857@gmail.com>
2024-12-17 15:02:51 +0900

    Style: 포스트 작성 페이지에서 카테고리 닫힘을 기본값으로 수정

--- src/components/Write/CategoryButton.tsx
+++ src/components/Write/CategoryButton.tsx
@@ -14,7 +14,7 @@ export default function CategoryButton({
   setChannel,
 }: CategoryDropdownProps) {
   const [channels, setChannels] = useState<Channel[]>();
-  const [isOpen, setIsOpen] = useState(!false);
+  const [isOpen, setIsOpen] = useState(false);
   const ref = useRef<HTMLElement>(null);
 
   const getChannels = async () => {
@@ -61,7 +61,7 @@ export default function CategoryButton({
               : "text-[#777] dark:text-gray-c8"
           )}
         >
-          {channel?.name ?? "카테고리"}
+          {channel?.name ?? "--- 선택 ---"}
         </p>
         <DropdownArrowIcon />
       </button>
