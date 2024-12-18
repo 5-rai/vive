38f63ba5fe3f621cbca997ec7ceeadaa01c78047
AAminha <dks4857@gmail.com>
2024-12-18 22:34:08 +0900

    Rename: 특정 사용자와의 메시지 내용 가져오는 훅 이름 변경

--- src/hooks/useGetConversationWithUser.ts
+++ src/hooks/useGetConversationWithUser.ts
@@ -1,20 +1,20 @@
 import { useEffect, useState } from "react";
 import { axiosInstance } from "../api/axios";
 
-const useGetMessagesWithUser = (userId: string) => {
-  const [messages, setMessages] = useState<Message[] | null>(null);
+const useGetConversationWithUser = (userId: string) => {
+  const [conversation, setConversation] = useState<Message[] | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const getMessagesWithUser = async () => {
+  const getConversationWithUser = async () => {
     if (!userId) return;
 
     try {
-      if (!messages) setLoading(true);
+      if (!conversation) setLoading(true);
       const { data } = await axiosInstance.get(`/messages`, {
         params: { userId },
       });
-      setMessages(data);
+      setConversation(data);
     } catch (err) {
       if (err instanceof Error) setError(true);
       console.error(err);
@@ -24,10 +24,10 @@ const useGetMessagesWithUser = (userId: string) => {
   };
 
   useEffect(() => {
-    getMessagesWithUser();
+    getConversationWithUser();
   }, [userId]);
 
-  return { messages, error, loading, refetch: getMessagesWithUser };
+  return { conversation, error, loading, refetch: getConversationWithUser };
 };
 
-export default useGetMessagesWithUser;
+export default useGetConversationWithUser;
