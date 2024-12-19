1341b6be652cbfa0665fd9ba8d4d6ccfe9f2ca27
AAminha <dks4857@gmail.com>
2024-12-19 16:53:56 +0900

    Remove: 불필요한 주석 삭제

--- src/hooks/useCarouselButtons.tsx
+++ src/hooks/useCarouselButtons.tsx
@@ -17,13 +17,11 @@ export const useCarouselButtons = (
   const onPrevButtonClick = useCallback(() => {
     if (!emblaApi) return;
     emblaApi.scrollPrev();
-    //emblaApi.scrollTo(-3); // 한 번에 3개의 슬라이드를 이동
   }, [emblaApi]);
 
   const onNextButtonClick = useCallback(() => {
     if (!emblaApi) return;
     emblaApi.scrollNext();
-    //emblaApi.scrollTo(-3); // 한 번에 3개의 슬라이드를 이동
   }, [emblaApi]);
 
   const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
