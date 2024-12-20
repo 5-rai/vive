63769b130a0e9fff4d2a618f6b5781890c79f921
AAminha <dks4857@gmail.com>
2024-12-20 15:57:35 +0900

    Fix: 로그인 완료 후 이전페이지로 이동하지 않고 홈으로 이동하는 문제 해결

--- src/layouts/NotPrivate.tsx
+++ src/layouts/NotPrivate.tsx
@@ -1,19 +1,13 @@
-import { Outlet, useNavigate } from "react-router";
+import { Navigate, Outlet } from "react-router";
 import { useAuthStore } from "../store/authStore";
-import { useEffect, useState } from "react";
 
 // 인증 안 된 사람만 접근 가능
 export default function NotPrivate() {
-  const navigate = useNavigate();
-  const [show, setIsShow] = useState(false);
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
 
-  useEffect(() => {
-    if (isLoggedIn) {
-      navigate("/");
-      return;
-    }
-    setIsShow(true);
-  }, [isLoggedIn]);
-  return <>{show && <Outlet />}</>;
+  if (isLoggedIn) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
 }
