6cf289b422ce6ba2d36c8948aadbd4045eddfd9d
AAminha <dks4857@gmail.com>
2024-12-20 15:12:29 +0900

    Style: 사이드 바 2개 영역의 기본 높이를 반반으로 수정

--- src/components/Sidebar/ChannelList.tsx
+++ src/components/Sidebar/ChannelList.tsx
@@ -6,18 +6,18 @@ export default function ChannelList() {
   const channels = useChannelStore((state) => state.channels);
 
   return (
-    <section className="h-fit">
+    <section className="h-1/2 max-h-fit overflow-y-hidden flex flex-col">
       <h2 className="border-b border-gray-22 dark:border-gray-ee/50 py-2 mb-2.5 dark:text-gray-ee">
         카테고리
       </h2>
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-1 overflow-y-auto custom-scrollbar">
         {channels.map((channel) => (
           <NavLink
             key={channel._id}
             to={`/channels/${channel.name}`}
             className={({ isActive }) =>
               twMerge(
-                "flex items-center h-11 px-7 py-1 rounded-lg transition-colors",
+                "flex items-center shrink-0 h-11 px-7 py-1 rounded-lg transition-colors",
                 isActive
                   ? "bg-primary dark:text-gray-22"
                   : "hover:bg-secondary dark:hover:text-gray-22"
