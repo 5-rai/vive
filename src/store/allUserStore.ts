a77ece8b2e678c5cd6d2c61b8cf2a9e9f0251bc8
AAminha <dks4857@gmail.com>
2024-12-21 00:32:17 +0900

    Fix: Storage 삭제 후 새로고침 시 발생하는 에러 해결 - subscribe를 통해 데이터를 가져올 때까지 대기

--- src/store/allUserStore.ts
+++ src/store/allUserStore.ts
@@ -4,10 +4,11 @@ import { getAllUsers } from "../api/user";
 interface AllUserStore {
   users: User[];
   fetchUsers: () => Promise<void>;
-  getUser: (id: string) => User | undefined;
+  getUser: (id: string) => Promise<User | undefined>;
+  getUsers: () => Promise<User[]>;
 }
 
-export const useAllUserStore = create<AllUserStore>((set, get) => ({
+export const useAllUserStore = create<AllUserStore>((set, get, api) => ({
   users: [],
   fetchUsers: async () => {
     try {
@@ -18,5 +19,32 @@ export const useAllUserStore = create<AllUserStore>((set, get) => ({
       console.error(err);
     }
   },
-  getUser: (id: string) => get().users.find((user) => user._id === id),
+  getUser: async (id: string) => {
+    if (get().users.length === 0) {
+      await new Promise<void>((resolve) => {
+        const unsubscribe = api.subscribe((state) => {
+          const users: User[] = state.users;
+          if (users.length > 0) {
+            unsubscribe();
+            resolve();
+          }
+        });
+      });
+    }
+    return get().users.find((user) => user._id === id);
+  },
+  getUsers: async () => {
+    if (get().users.length === 0) {
+      await new Promise<void>((resolve) => {
+        const unsubscribe = api.subscribe((state) => {
+          const users: User[] = state.users;
+          if (users.length > 0) {
+            unsubscribe();
+            resolve();
+          }
+        });
+      });
+    }
+    return get().users;
+  },
 }));
