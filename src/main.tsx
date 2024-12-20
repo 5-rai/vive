4ef3ec282076c22523371190e0871a68cd2fca73
AAminha <dks4857@gmail.com>
2024-12-20 09:31:54 +0900

    Chore: base 수정 및 사이트명 추가

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/project-2">
+    <BrowserRouter basename="/vive">
       <App />
     </BrowserRouter>
   </StrictMode>
