bde913b9f1579862a1d50e8e3730484191244ad4
AAminha <dks4857@gmail.com>
2024-12-21 18:58:49 +0900

    Style: 헤더 SearchBar에 focus 색상 반영

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -5,6 +5,9 @@ import { useNavigate } from "react-router";
 export default function SearchBar() {
   const navigate = useNavigate();
   const [keyword, setKeyword] = useState("");
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
+  const toggledInputFocused = () => setIsInputFocused((prev) => !prev);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -14,7 +17,7 @@ export default function SearchBar() {
 
   return (
     <form
-      className="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[514px] h-[43px] gap-2.5 px-[25px] py-[5px] rounded-full border border-gray-54 dark:border-gray-c8/50 bg-white dark:bg-white/10"
+      className="flex justify-center items-center w-[514px] h-[43px] px-6 rounded-full border border-gray-54 dark:border-gray-c8/50 focus-within:border-primary bg-white dark:bg-white/10"
       onSubmit={handleSubmit}
     >
       <input
@@ -23,9 +26,14 @@ export default function SearchBar() {
         className="w-full h-full pr-3 bg-transparent focus:outline-none dark:text-gray-ee"
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
+        onFocus={toggledInputFocused}
+        onBlur={toggledInputFocused}
       />
       <button className="flex items-center justify-center w-[36px] h-[36px]">
-        <SearchIcon className="w-[19px] h-[19px]" />
+        <SearchIcon
+          className="w-[19px] h-[19px]"
+          color={isInputFocused ? "#FCC404" : undefined}
+        />
       </button>
     </form>
   );
