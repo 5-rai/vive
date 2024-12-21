1adc97917553165dd7b4628d33415ec4532b674a
AAminha <dks4857@gmail.com>
2024-12-22 03:12:28 +0900

    Refactor: 태그 속성 보완

--- src/components/Header/LogInButton.tsx
+++ src/components/Header/LogInButton.tsx
@@ -3,7 +3,10 @@ import { Link } from "react-router";
 export default function LogInButton() {
   return (
     <Link to="/login">
-      <button className="w-[106px] h-10 rounded-full primary-btn text-base font-medium">
+      <button
+        type="button"
+        className="w-[106px] h-10 rounded-full primary-btn text-base font-medium"
+      >
         로그인
       </button>
     </Link>
