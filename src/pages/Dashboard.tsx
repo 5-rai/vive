77943c2a6c9d40b21eb1614850cdff24e6043793
AAminha <dks4857@gmail.com>
2024-12-22 14:43:42 +0900

    Style: Dashboard 페이지 하단 공백 수정

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -68,8 +68,8 @@ export default function Dashboard() {
   }
 
   return (
-    <div className="mx-auto w-[934px]">
-      <div className="flex items-center justify-between my-10">
+    <div className="mx-auto w-[934px] my-10">
+      <div className="flex items-center justify-between mb-10">
         <h2 className="text-[32px] font-bold">{channelName}</h2>
         <SortButton
           currentSort={sortOption}
